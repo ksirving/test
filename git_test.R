042370e281cba1df834379445bbe14871203e438
ksirving <32795054+ksirving@users.noreply.github.com>
2019-09-24 13:41:34 -0700

    just testing

---
+++ git_test.R
@@ -0,0 +1,11 @@
+### test on git hub
+
+install.packages("raster")
+library(raster)
+
+### niche mapper 
+
+install.packages("NicheMapR")
+library("NicheMapR")
+install.packages(c("raster", "sp", "ncdf", "XML", "dismo","chron", "rgdal", "zoo"))
+library(c("raster", "sp", "ncdf", "XML", "dismo","chron", "rgdal", "zoo"))
